e68795f2224da576fdc68493ae232bc686fdf22f
Martha Nieto <m27842n@outlook.com>
2022-12-26 20:08:34 -0800

    - update: main - added seeder for testing data in UI.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\TagController;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'tags' => \App\Models\Tag::get(),
+        'tags' => Tag::get(),
     ]);
 });
 
